b15746b300ecdd70fc20f907762e3ce376b724ca
Keith Cirkel <keithamus@users.noreply.github.com>
2018-09-14 12:41:22 +0100

    test: refactor tests, drop mock-fs

--- index.test.js
+++ index.test.js
@@ -1,51 +1,36 @@
 'use strict';
 
-const mockfs = require('mock-fs');
-const fs = require('fs');
 const postcss = require('postcss');
-const path = require('path');
-const os = require('os');
-
+const {join} = require('path');
+const { tmpdir } = require('os');
+const { readFileSync, writeFileSync, readdirSync, unlinkSync } = require('fs');
 const utils = require('./utils');
 const plugin = require('./');
 
 const CSS_FILES = {
-    './in': {
-        'file01.css': '.a {}',
-        'file02.css': '.b {background-color: #fff}',
-        'file03.scss': '.c {}'
-    }
+    'file01.css': '.a {}',
+    'file02.css': '.b {background-color: #fff}',
+    'file03.scss': '.c {}'
 };
 
 beforeEach(() => {
-    mockfs(CSS_FILES);
-});
+    Object.keys(CSS_FILES).forEach(file => writeFileSync(join(tmpdir(), file), CSS_FILES[file]))
+})
 
 afterEach(() => {
-    mockfs.restore();
-});
-
-function content(file) {
-    return fs.readFileSync(file).toString();
-}
-
-function run(file, opts) {
-    return postcss([ plugin(opts)]).process(content('./' + file), {
-        from: file,
-        to: file
-    }).then(result => {
-        const hash = utils.hash(content(file), opts.algorithm, opts.trim);
-
-        expect(result.opts.to).toMatch(new RegExp(hash));
-        expect(result.warnings().length).toBe(0);
+    Object.keys(CSS_FILES).concat('manifest.json').forEach(file => {
+        try {
+            unlinkSync(join(tmpdir(), file));
+        } catch (e) {
+        }
     });
-}
+})
 
 test('hash module', () => {
-    const file = './in/file01.css';
+    const file = join(tmpdir(), 'file01.css');
 
     expect.assertions(1);
-    expect(utils.hash(content(file), 'sha256', 5)).toHaveLength(5);
+    expect(utils.hash(readFileSync(file, 'utf-8'), 'sha256', 5)).toHaveLength(5);
 });
 
 test('defaultName module', () => {
@@ -60,30 +45,37 @@ test('defaultName module', () => {
 });
 
 test('rename module', () => {
-    const file = './in/file01.css';
+    const file = join(tmpdir(), 'file01.css');
     const opts = { algorithm: 'sha256', trim: 5, name: utils.defaultName };
-    const hash = utils.hash(content(file), opts.algorithm, opts.trim);
+    const hash = utils.hash(readFileSync(file, 'utf-8'), opts.algorithm, opts.trim);
 
     expect.assertions(1);
-    expect(utils.rename(file, content(file), opts)).toMatch(new RegExp(hash));
+    expect(utils.rename(file, readFileSync(file, 'utf-8'), opts)).toMatch(new RegExp(hash));
 });
 
 test('data module', () => {
-    const file = './in/file01.css';
+    const file = join(tmpdir(), 'file01.css');
     const opts = { algorithm: 'md5', trim: 5, name: utils.defaultName };
-    const renamedFile = utils.rename(file, content(file), opts);
+    const renamedFile = utils.rename(file, readFileSync(file, 'utf-8'), opts);
 
     expect.assertions(1);
     expect(utils.data(file, renamedFile)).toBeInstanceOf(Object);
 });
 
 test('plugin', () => {
-    const root = './in';
-    const opts = { algorithm: 'md5', trim: 5, manifest: path.join(os.tmpdir(), 'manifest.json') };
+    const opts = { algorithm: 'md5', trim: 5, manifest: join(tmpdir(), 'manifest.json') };
 
-    const files = fs.readdirSync(root);
+    const files = readdirSync(tmpdir()).filter(name => /css$/.test(name));
 
-    for (var file in files) {
-        run(path.join(root, files[file]), opts);
-    }
+    return Promise.all(files.map(file => {
+        return postcss([ plugin(opts)]).process(readFileSync(join(tmpdir(), file), 'utf-8'), {
+            from: file,
+            to: file
+        }).then(result => {
+            const hash = utils.hash(readFileSync(join(tmpdir(), file), 'utf-8'), opts.algorithm, opts.trim);
+
+            expect(result.opts.to).toMatch(new RegExp(hash));
+            expect(result.warnings().length).toBe(0);
+        });
+    }));
 });
